61b53292a02a3f8b1ecf8ec57066f72cf2787cd7
Bear_Jay <jaesuki98@gmail.com>
2022-01-26 16:32:57 +0900

    done with fit test

--- modules/fit_test.js
+++ modules/fit_test.js
@@ -7,7 +7,6 @@ export default class FitGame {
 
         this.$container = $(query);
         this.$answerRow = $(query).children("#answer");
-        this.$optionRow = $(query).children("#option");
 
         this.initGame();
 
@@ -16,26 +15,53 @@ export default class FitGame {
         this.fitGameScore = 0;
     }
     initGame() {
-        this.$answerRow.append(`<div class="col p-3"><div class="color" style="background-color: ${GAMEINFO.givenArr[0]};"></div></div>`);
+        this.$answerRow.append(`<div class="col p-3">
+        <div class="color" style="background-color: ${GAMEINFO.givenArr[0]}; display:none;"></div>
+        </div>`);
+        const NumOfRow = parseInt(GAMEINFO.optionArr.length / 2);
+        for (let i = 0; i < NumOfRow; i++) {
+            this.$container.append(`<div id="option-row-${i}" class="row row-cols-2" style="height: ${NumOfRow==1?"20%":"15%"};"></div>`);
+        }
         GAMEINFO.optionArr.forEach((el, i) => {
-            this.$optionRow.append(`<div class="col p-2 h-25 column-${i%2} mx-auto my-auto">
-            <div class="color" style="background-color: ${el};"></div>
+            $(`#option-row-${parseInt(i/2)}`).append(`<div class="column-${i%2} col p-2 mx-auto">
+            <div class="color" style="background-color: ${el};  display:none;"></div>
             </div>`);
-            this.$optionRow.find("div.color").off().on({
+            $(`#option-row-${parseInt(i/2)} > .column-${i%2} > .color`).off().on({
                 "pointerdown": this.onDown,
                 "pointerenter": this.onEnter,
                 "pointerleave": this.onLeave,
                 "pointerup": this.onUp
             }, null, {
                 app: this,
-                animate: (col) => {
-                    col.toggleClass("h-25 h-50 col col-7");
-                    const divClassColIndex = col.attr("class").indexOf("column-") + 7;
-                    if (col.attr("class").slice(divClassColIndex, divClassColIndex + 1) == '0') col.next().toggleClass("h-25 h-50 col col-5");
-                    else col.prev().toggleClass("h-25 h-50 col col-5");
-                }
+                animateIn: (col) => {
+                    col.parent().attr("style", "height: 23%;");
+                    col.parent().siblings("#option-row-0,#option-row-1,#option-row-2,#option-row-3").attr("style", "height: 10%;");
+                    const findClassOfCol = col.attr("class").indexOf("column-") + 7;
+                    const columnIndex = col.attr("class").slice(findClassOfCol, findClassOfCol + 1);
+                    if (columnIndex == '0') {
+                        $(".column-0").toggleClass("col col-7");
+                        $(".column-1").toggleClass("col col-5");
+                    } else {
+                        $(".column-1").toggleClass("col col-7");
+                        $(".column-0").toggleClass("col col-5");
+                    }
+                },
+                animateOut: (col) => {
+                    col.parent().attr("style", `height: ${NumOfRow==1?"20%":"15%"};`);
+                    col.parent().siblings("#option-row-0,#option-row-1,#option-row-2,#option-row-3").attr("style", `height: ${NumOfRow==1?"20%":"15%"};`);
+                    const findClassOfCol = col.attr("class").indexOf("column-") + 7;
+                    const columnIndex = col.attr("class").slice(findClassOfCol, findClassOfCol + 1);
+                    if (columnIndex == '0') {
+                        $(".column-0").toggleClass("col col-7");
+                        $(".column-1").toggleClass("col col-5");
+                    } else {
+                        $(".column-1").toggleClass("col col-7");
+                        $(".column-0").toggleClass("col col-5");
+                    }
+                },
             })
         });
+        $(".color").fadeIn(300);
     }
 
     /* event handler jQuery code */
@@ -49,11 +75,11 @@ export default class FitGame {
     onEnter(e) {
         const enteredCol = $(e.target).parent()
         e.data.enteredCol = enteredCol;
-        e.data.animate(enteredCol);
+        e.data.animateIn(enteredCol);
     }
     onLeave(e) {
         const enteredCol = e.data.enteredCol;
-        e.data.animate(enteredCol);
+        e.data.animateOut(enteredCol);
         e.data.clickedQuery = undefined;
     }
     onUp(e) {
@@ -66,11 +92,23 @@ export default class FitGame {
     /* event handler jQuery code ends */
 
     scoreNnextGame() {
-        this.$optionRow.find("div.color").off();
+        $(".color").off();
         this.perfectScore += GAMEINFO.optionArr.length / 2;
         const colorStyleIndex = $(this.clickedQuery).attr("style").indexOf("background-color:") + 18;
-        console.log($(this.clickedQuery).attr("style").slice(colorStyleIndex, colorStyleIndex + 7));
-        if ($(this.clickedQuery).attr("style").slice(colorStyleIndex, colorStyleIndex + 7) == GAMEINFO.givenArr[0]) {
+        const colorStyleEnd = $(this.clickedQuery).attr("style").indexOf(");") + 1;
+        let clickedColor = 0;
+        let index = 0;
+        $(this.clickedQuery)
+            .attr("style")
+            .slice(colorStyleIndex, colorStyleEnd)
+            .split(/\D/)
+            .forEach((RGBs, _i) => {
+                if (RGBs) {
+                    clickedColor += RGBs * Math.pow(256, 2 - index);
+                    index += 1;
+                }
+            })
+        if (clickedColor == GAMEINFO.givenArr[0]) {
             this.fitGameScore += GAMEINFO.optionArr.length / 2;
         }
         this.currentstage += 1;
@@ -78,16 +116,16 @@ export default class FitGame {
             GAMEINFO.initCurrentGame("fit", this.currentstage);
         } catch (err) {
             if (err == "End Of Stage") {
-                this.$currentClickEvent.off("click");
-                this.$contextRow.children().fadeOut(300);
                 this.gradeFitGame();
                 return;
             }
         }
+        this.$answerRow.children().fadeOut(250);
+        this.$container.children("#option-row-0,#option-row-1,#option-row-2,#option-row-3").fadeOut(250);
         this.$answerRow.children().remove();
-        this.$optionRow.children().remove();
-
+        this.$container.children("#option-row-0,#option-row-1,#option-row-2,#option-row-3").remove();
         this.initGame();
+
     }
     gradeFitGame() {
 
