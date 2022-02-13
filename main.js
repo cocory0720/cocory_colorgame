697e18d6ef6fca4627f283f41f8bfce915790ebd
Bear_Jay <jaesuki98@gmail.com>
2022-02-13 16:53:09 +0900

    merge from mia branch

--- main.js
+++ main.js
@@ -21,11 +21,6 @@ window.addEventListener("DOMContentLoaded", function() {
 
 
 
-/** 테스트 순서 */
-const SERIES = ["main", "fit", "value", "chroma", "hue", "end"];
-
-
-
 /* article 페이드 인/아웃 지속시간 */
 const FADE_OUT_TIME = 300; // 페이드 아웃
 const DELAY_FOR_SUBMITTING = 500; // 테스트 종료시 딜레이
@@ -38,7 +33,7 @@ const FADE_IN_TIME = 600; // 페이드 인
  *  main.js 에서 호출하는 각 클래스의 메소드는 다음과 같음
  *  1. currentContext.canvas : 테스트를 구현할 HTML객체를 담은 [변수 (HTML쿼리)]
  *  2. currentContext.grade???Game : 각 테스트 제출 후 시간에 따른 점수 부여를 위한 [함수], submit()에서 사용 
- *  3. currentContext.delAllReq : 테스트 종료 후, 모듈에서 등록한 이벤트리스너와 Animation Requestes를 제거
+ *  3. currentContext.delAllReq : 테스트 종료 후, 모듈에서 등록한 이벤트리스너와 Animation Requestes를 제거하기위한 [함수]
  */
 let currentContext;
 
@@ -57,7 +52,6 @@ initBtns("start-main"); // 첫 페이지를 위함
 
 
 /** 다음 article로 넘어가기 위한 함수.
- * 다음 article이 현재 HTML파일 내에 존재하지 않을 경우, 다음 게임에 해당하는 HTML/CSS 파일을 읽어옴.
  * @param {object} e 다음 페이지로 넘어가기 위해 발생한 이벤트, 혹은 그것의 타겟
  */
 function showNextArticle(e) {
@@ -86,50 +80,48 @@ function showNextArticle(e) {
                  */
 
 
-                $(document).off("click"); // document내 이벤트리스너 청소
+                /* 브라우저가 캐시를 사용하는 경우 동작하지 않음 */
+                // if (GAMEINFO.currentGame == undefined) {
+                //     // 현재 첫 테스트를 시작하지 않았을 경우, 첫 번째 테스트 진입
+
+                //     // 첫번째 테스트에 해당하는 파일의 CSS 링크
+                //     $("head").append(
+                //         $(`<link rel="stylesheet" href="./${SERIES[1]}/${SERIES[1]}_style.css">`)
+                //     );
+
+                //     // 첫번째 테스트에 해당하는 파일의 HTML의 article 태그들 파싱
+                //     $("main").load(
+                //         `./${SERIES[1]}/${SERIES[1]}_index.html #test-app article`,
+                //         function(_resp, status, xhr) {
+                //             if (status == "error") window.alert(xhr.status + " " + xhr.statusText);
+                //             $("article:first").fadeIn(FADE_IN_TIME);
+                //             initBtns($("article:first").attr("id"));
+                //         }
+                //     );
 
 
-                if (GAMEINFO.currentGame == undefined) {
-                    // 현재 첫 테스트를 시작하지 않았을 경우, 첫 번째 테스트 진입
+                // } else {
+                //     // 테스트를 시작한 경우, 그 다음 테스트 진입
 
-                    // 첫번째 테스트에 해당하는 파일의 CSS 링크
-                    $("head").append(
-                        $(`<link rel="stylesheet" href="./${SERIES[1]}/${SERIES[1]}_style.css">`)
-                    );
+                //     const currentSeriesIndex = SERIES.indexOf(GAMEINFO.currentGame); // 현재 테스트 파일명과 인덱스
 
-                    // 첫번째 테스트에 해당하는 파일의 HTML의 article 태그들 파싱
-                    $("main").load(
-                        `./${SERIES[1]}/${SERIES[1]}_index.html #test-app article`,
-                        function(_resp, status, xhr) {
-                            if (status == "error") window.alert(xhr.status + " " + xhr.statusText);
-                            $("article:first").fadeIn(FADE_IN_TIME);
-                            initBtns($("article:first").attr("id"));
-                        }
-                    );
+                //     // 현재 CSS링크를 지우고
+                //     $(`link[href = "./${SERIES[currentSeriesIndex]}/${SERIES[currentSeriesIndex]}_style.css"]`).remove();
 
+                //     $("head").append( // 첫번째 테스트에 해당하는 파일의 CSS 링크
+                //         $(`<link rel="stylesheet" href="./${SERIES[currentSeriesIndex + 1]}/${SERIES[currentSeriesIndex + 1]}_style.css">`)
+                //     );
 
-                } else {
-                    // 테스트를 시작한 경우, 그 다음 테스트 진입
-
-                    const currentSeriesIndex = SERIES.indexOf(GAMEINFO.currentGame); // 현재 테스트 파일명과 인덱스
-
-                    // 현재 CSS링크를 지우고
-                    $(`link[href = "./${SERIES[currentSeriesIndex]}/${SERIES[currentSeriesIndex]}_style.css"]`).remove();
-
-                    $("head").append( // 첫번째 테스트에 해당하는 파일의 CSS 링크
-                        $(`<link rel="stylesheet" href="./${SERIES[currentSeriesIndex + 1]}/${SERIES[currentSeriesIndex + 1]}_style.css">`)
-                    );
-
-                    // 첫번째 테스트에 해당하는 파일의 HTML의 article 태그들 파싱
-                    $("main").load(
-                        `./${SERIES[currentSeriesIndex + 1]}/${SERIES[currentSeriesIndex + 1]}_index.html #test-app article`,
-                        function(_resp, status, xhr) {
-                            if (status == "error") window.alert(xhr.status + " " + xhr.statusText);
-                            $("article:first").fadeIn(FADE_IN_TIME);
-                            initBtns($("article:first").attr("id"));
-                        }
-                    );
-                }
+                //     // 첫번째 테스트에 해당하는 파일의 HTML의 article 태그들 파싱
+                //     $("main").load(
+                //         `./${SERIES[currentSeriesIndex + 1]}/${SERIES[currentSeriesIndex + 1]}_index.html #test-app article`,
+                //         function(_resp, status, xhr) {
+                //             if (status == "error") window.alert(xhr.status + " " + xhr.statusText);
+                //             $("article:first").fadeIn(FADE_IN_TIME);
+                //             initBtns($("article:first").attr("id"));
+                //         }
+                //     );
+                // }
 
 
             } else {
@@ -137,10 +129,13 @@ function showNextArticle(e) {
                  *  또한 그 article이 테스트를 구현하는 경우, 
                  */
 
+                $(document).off("click"); // document내 이벤트리스너 청소
+
+
                 switch ($clickedArticle.next().attr("id")) {
                     /** 다음 article의 id값에 따라
                      *  1. 각 모듈에서 구현된 테스트를 불러오기
-                     *  2. 테스트 시작하기
+                     *  2. 테스트 시작하기(타이머 작동)
                      *  3. 배경 변경하기 등
                      */
 
@@ -248,10 +243,7 @@ function showNextArticle(e) {
 // class = "action-submit" : Score current result
 let remainTime = 0; /****** 남은 시간 ******/
 function startGame() {
-    $(document).one("click", ".action-submit", function() {
-        submit(remainTime);
-        clearInterval(timer);
-    });
+
     showRemainTime();
     remainTime = 0;
     remainTime += GAMEINFO.timeLimit + FADE_IN_TIME / 1000;
@@ -264,6 +256,10 @@ function startGame() {
         remainTime -= 0.01;
     }, 10);
 
+    $(document).one("click", ".action-submit", function() {
+        submit(remainTime);
+        clearInterval(timer);
+    });
 }
 
 function showRemainTime() {
